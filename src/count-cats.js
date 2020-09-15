1326c5fa0c6a1bdfed62d5d019680eced7460764
Andrei Roh <65450338+andrei-roh@users.noreply.github.com>
2020-09-15 15:24:47 +0300

    refactor: code update
    Fixed code mistakes and added new function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,10 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(backyard){
-  let number = 0;
-  for (i = 0; i < backyard.length; i++){
-    if (backyard[i] === '^^')
-    {number += 1}
-  }
-  return number
+module.exports = function countCats(matrix){
+	  let arr = matrix.flat()
+	  let number = 0;
+	  for (i = 0; i < arr.length; i++){
+	    if (arr[i] === '^^') {
+        {number += 1}
+      }
+    }
+    return number;
 }
