9ccddc45774dada04126aa233abbd939d6061e1e
Andrei Roh <65450338+andrei-roh@users.noreply.github.com>
2020-09-16 15:24:54 +0300

    refactor: update dream-team.js
    Reducing the number of calls in function

--- src/dream-team.js
+++ src/dream-team.js
@@ -10,30 +10,24 @@ module.exports = function createDreamTeam(members) {
       if (/[0-9]/.test(members[i]) || typeof(members[i]) !== 'string') {
         members.splice(i, 1)
       }
+    }
+    for (i = 0; i < members.length; i++) {
       if (/[0-9]/.test(members[i]) || typeof(members[i]) !== 'string') {
         members.splice(i, 1)
       }
-      if (/[0-9]/.test(members[i]) || typeof(members[i]) !== 'string') {
-        members.splice(i, 1)
-      }
-      if (/[0-9]/.test(members[i]) || typeof(members[i]) !== 'string') {
-        members.splice(i, 1)
-      }
-      if (/[0-9]/.test(members[i]) || typeof(members[i]) !== 'string') {
-        members.splice(i, 1)
-      }
+    }
+    for (i = 0; i < members.length; i++) {
       if(typeof(members[i]) == 'string'){
         members[i] = members[i].trim().toUpperCase()
       }
     }
     members = members.sort().flat();
-  for (i = 0; i < members.length; i++) {
-      if (/[0-9]/.test(members[i])) {
-      members.splice(i, 1)
-      }
-      teamName += members[i].charAt(0)   
-   }
-   return teamName;
-   }
+    for (i = 0; i < members.length; i++) {
+        if (/[0-9]/.test(members[i])) {
+        members.splice(i, 1)
+        }
+        teamName += members[i].charAt(0)   
+    }
+    return teamName
   }
-};
+}
