e331ec3afcfa485ecb5a4156e60e8663b18ea4b8
Andrei Roh <65450338+andrei-roh@users.noreply.github.com>
2020-09-16 15:18:47 +0300

    init: start dream-team task
    Added createDreamTeam funciton

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let teamName = [];
+  if (!Array.isArray(members)){
+    return false
+  }
+  else {
+    for (i = 0; i < members.length; i++) {
+      if (/[0-9]/.test(members[i]) || typeof(members[i]) !== 'string') {
+        members.splice(i, 1)
+      }
+      if (/[0-9]/.test(members[i]) || typeof(members[i]) !== 'string') {
+        members.splice(i, 1)
+      }
+      if (/[0-9]/.test(members[i]) || typeof(members[i]) !== 'string') {
+        members.splice(i, 1)
+      }
+      if (/[0-9]/.test(members[i]) || typeof(members[i]) !== 'string') {
+        members.splice(i, 1)
+      }
+      if (/[0-9]/.test(members[i]) || typeof(members[i]) !== 'string') {
+        members.splice(i, 1)
+      }
+      if(typeof(members[i]) == 'string'){
+        members[i] = members[i].trim().toUpperCase()
+      }
+    }
+    members = members.sort().flat();
+  for (i = 0; i < members.length; i++) {
+      if (/[0-9]/.test(members[i])) {
+      members.splice(i, 1)
+      }
+      teamName += members[i].charAt(0)   
+   }
+   return teamName;
+   }
+  }
 };
