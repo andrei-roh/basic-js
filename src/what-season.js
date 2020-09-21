beffab487b778118855623883b8b67309a56988b
Andrei Roh <65450338+andrei-roh@users.noreply.github.com>
2020-09-21 10:56:51 +0300

    Initi: start what-season task
    Added what-season function

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (!date) {
+    return 'Unable to determine the time of year!'
+    }
+    else if (!date.getTime()) {
+      return new Error
+      };
+  let whatSeason = new Date(date).toString().split(' ')
+  if (whatSeason[1] == 'Dec' || whatSeason[1] == 'Jan' || whatSeason[1] == 'Feb') {
+    return 'winter'
+  }
+    else if (whatSeason[1] == 'Mar' || whatSeason[1] == 'Apr' || whatSeason[1] == 'May') {
+      return 'spring'
+    }
+    else if (whatSeason[1] == 'Jun' || whatSeason[1] == 'Jul' || whatSeason[1] == 'Aug') {
+      return 'summer'
+    }
+    else if (whatSeason[1] == 'Sep' || whatSeason[1] == 'Oct' || whatSeason[1] == 'Nov') {
+      return 'autumn'
+    }
 };
