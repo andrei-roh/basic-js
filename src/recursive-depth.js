10ee601e4cea53a8c8d65efaa478bfcc74c9e598
Andrei Roh <andrei_roh@tut.by>
2020-09-22 11:12:27 +0300

    Refactor: start recursive-depth task
    Added calculateDepth function for calculate level of depth for array

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let arrDepth = 0;
+    arr.forEach(element => {
+      if (Array.isArray(element)){
+      arrDepth = Math.max(this.calculateDepth(element), arrDepth);
+      }
+    })
+    return arrDepth + 1;
   }
 };
