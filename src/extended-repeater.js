6083df5355f5272a1eb22e2cb2fdcd36e2308b62
Andrei Roh <andrei_roh@tut.by>
2020-10-06 11:17:01 +0300

    init: extended-repeater solution
    Added repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let strRepeater = str + '';
+  if (options) {
+    const {repeatTimes, addition, additionRepeatTimes} = options;
+    let {separator, additionSeparator} = options;
+    if (separator === undefined) {
+      separator = '+';
+    }
+    if (!additionSeparator === undefined) {
+      additionSeparator = '|';
+    }
+    function repeat(str, repeater, separator) {
+      let strRepeater =  str;
+      for (let i = 0; i < (repeater ? repeater - 1 : 0); i += 1 ) {
+        strRepeater += separator + str;
+      }
+      return strRepeater;
+    }
+    if (addition !== undefined) {
+      strRepeater += repeat(addition, additionRepeatTimes, additionSeparator);
+    }
+    return repeat(strRepeater, repeatTimes, separator);
+  }
+  return strRepeater;
 };
-  
