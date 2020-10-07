e71e8347be6ff4f20c01162eed7e4065a7a05598
Andrei Roh <andrei_roh@tut.by>
2020-10-07 20:07:07 +0300

    feat: change transform-array solution
    Added 2 arrays from to fill with data

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,25 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if(!Array.isArray(arr)) throw new Error
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[0] === '--discard-prev') {arr.splice([i], 1)}
-    else if (arr[0] === '--discard-next') {}
-    else if (arr[0] === '--double-next') {}
-    else if (arr[0] === '--double-prev') {arr.splice([i], 1)}
-    else if (arr[i] === 'true') {arr.splice([i], 1)}
-    else if (arr[i] === '--discard-next') {arr.splice([i], 2)}
-    else if (arr[i] === '--discard-prev') {arr.splice([i - 1], 2)}
-    else if (arr[i] === '--double-next') {arr.splice([i], 1, arr[i + 1])}
-    else if (arr[i] === '--double-prev') {arr.splice([i], 1, arr[i - 1])}
-    else if (arr[arr.length - 1] === '--double-next') {arr.splice([arr.length - 1], 1)}
-    else if (arr[arr.length - 1] === '--discard-prev') {arr.splice([arr.length - 1], 1)}
+  if(!(Array.isArray(arr))) {
+    throw new Error;
   }
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === '--discard-prev') {arr.splice([i], 1)}
-    else if (arr[i] === '--double-prev') {arr.splice([i], 1)}
-    else if (arr[i] === '--double-next') {arr.splice([i], 1)}
-    else if (arr[i] === '--discard-next') {arr.splice([i], 1)}
+  else if(arr.length == 0) {
+    return [];
   }
-  return arr
+  let convertArray = [];
+  let removePart = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--discard-next") {
+      removePart.push(++i);
+    } else if (arr[i] === "--discard-prev") {
+      if (convertArray.length > 0 && removePart.indexOf(i - 1) === -1) {
+        convertArray = convertArray.slice(0, convertArray.length - 1);
+        removePart.push(i - 1);
+      }
+    } else if (arr[i] === "--double-next") {
+      if (++i < arr.length) {
+        convertArray.push(arr[i], arr[i]);
+      }
+    } else if (arr[i] === "--double-prev") {
+      if (i > 0 && removePart.indexOf(i - 1) === -1) {
+        convertArray.push(arr[i - 1]);
+      }
+    } else {
+      convertArray.push(arr[i]);
+    }
+  }
+  return convertArray;
 };
