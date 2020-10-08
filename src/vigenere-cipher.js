248938c0d17c781e2f2799e4eedb6a7471bcd490
Andrei Roh <andrei_roh@tut.by>
2020-10-08 09:31:48 +0300

    init: solution vigenere-cipher task
    Added class VigenereCipheringMachine for encrypt and decrypt words.

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,70 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(inverted = true) {
+        this.inverted = inverted;
+        this.tabulaRecta = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        this.position = 0;
+    }
+
+    encrypt(string, key) {
+      let result = '';
+      if (!string || !key){
+        throw new Error();
+      }
+
+      let msg = string.toUpperCase();
+      key = key.toUpperCase();
+
+      while (key.length < msg.length) {
+        key += key;
+      }
+
+      for (let i = 0, j = 0; i < msg.length; i++ , j++) {
+        if (this.tabulaRecta.includes(msg[i])) {
+          this.position = this.tabulaRecta.indexOf(msg[i]) + this.tabulaRecta.indexOf(key[j])
+            if (this.position >= this.tabulaRecta.length) {
+              this.position -= this.tabulaRecta.length;
+            }
+          result += this.tabulaRecta[this.position];
+          } else {
+              result += msg[i];
+              j--;
+            }
+      }
+
+      return (this.inverted) ? result : result.split('').reverse().join('')
+    };
+
+    decrypt(string, key) {
+      let result = '';
+
+      if (!string || !key){
+        throw new Error();
+      }
+
+      let msg = string.toUpperCase();
+      key = key.toUpperCase();
+
+      while (key.length < msg.length) {
+        key += key;
+      }
+
+      for (let i = 0, j = 0; i < msg.length; i++ , j++) {
+        if (this.tabulaRecta.includes(msg[i])) {
+          this.position = this.tabulaRecta.indexOf(msg[i]) - this.tabulaRecta.indexOf(key[j])
+            if (this.position < 0) {
+              this.position += this.tabulaRecta.length;
+            }
+            result += this.tabulaRecta[this.position];
+            } else {
+              result += msg[i];
+              j--;
+            }
+      }
+
+      return (this.inverted) ? result : result.split('').reverse().join('')
+    };
 }
 
 module.exports = VigenereCipheringMachine;
